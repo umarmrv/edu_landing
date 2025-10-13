b0a1c689eb6397a54db9bc6d9848912fbab43197
Asadbek Nishonov <v1aly.personal@gmail.com>
2025-10-13 21:10:48 +0500

    #1 Changes in models.py

--- edu_landing/models.py
+++ edu_landing/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class SiteSettings(models.Model):
+    site_name = models.CharField("Название сайта", max_length=100)
+    logo = models.ImageField("Логотип", upload_to="logos/", blank=True, null=True)
+    phone = models.CharField("Телефон", max_length=50, blank=True)
+    email = models.EmailField("Email", blank=True)
+    address = models.CharField("Адрес", max_length=200, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.pk = 1
+        super(SiteSettings, self).save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        pass
+
+    class Meta:
+        verbose_name = "Настройки сайта"
+        verbose_name_plural = "Настройки сайта"
+    
+    def __str__(self):
+        return "Настройки сайта"
