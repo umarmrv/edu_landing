10f18095d88018bb4ecb2315c44612fc5df4a62b
Asadbek Nishonov <v1aly.personal@gmail.com>
2025-10-14 20:14:56 +0500

    I've added all pages to views and urls

--- base/urls.py
+++ base/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', include("edu_landing.urls")),
+    path('', include("edu_landing.urls")),
 ]
 
 if settings.DEBUG:
